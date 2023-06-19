f0600ef893e32f80e446c3f7ffd516a4fceed1ba
erick odero <erickarthurs@gmail.com>
2023-06-19 11:30:02 -0500

    implemented image upload with multer for children

--- src/routes/childrenRoute.js
+++ src/routes/childrenRoute.js
@@ -5,12 +5,12 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 const childrenController = require('../controllers/userController/childrenController');
 
-router.post('/createChild', authController.createChild);
+router.post('/createChild',authController.restrict, authController.createChild);
 router.post('/loginChild', authController.loginChild);
 
 router.route('/').get(childrenController.getAllChildren);
 
-router.patch('/updateMe', childrenController.uploadImage, childrenController.updateMe);
+router.patch('/updateMe', authController.restrict, childrenController.uploadImage, childrenController.updateMe);
 
 router.route('/:id').get(childrenController.getChildById).patch(childrenController.editChild).delete(childrenController.deleteChild);
 
