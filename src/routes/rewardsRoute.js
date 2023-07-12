d366616696a5f491ff381b813cfcd20bce02384e
erick odero <erickarthurs@gmail.com>
2023-07-11 21:25:43 -0500

    modified rewards and task routes

--- src/routes/rewardsRoute.js
+++ src/routes/rewardsRoute.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const authController = require('../controllers/authController');
 
 const rewardsController = require('../controllers/rewardsController');
 
 const router = express.Router();
 
-router.route('/').post(rewardsController.createReward).get(rewardsController.getAllRewards)
+router.route('/').post(authController.restrictToParents, rewardsController.createReward).get(rewardsController.getAllRewards)
 
-router.route('/:id').get(rewardsController.getReward).patch(rewardsController.editReward)
-.delete(rewardsController.deleteReward);
+router.route('/:id').get(rewardsController.getReward).patch(authController.restrictToParents, rewardsController.editReward)
+.delete(authController.restrictToParents, rewardsController.deleteReward);
 
 
 
